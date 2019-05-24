33adae6632f2bcec47670e19acbd40080bcaf6b6
Inolas <salonibuddhadeo@gmail.com>
2019-05-24 01:48:08 -0700

    live search;to-do : resolve an error

--- js/scriptQ.js
+++ js/scriptQ.js
@@ -1,14 +1,23 @@
+$('#search').keyup(function () {
+    var searchField = $('search').val();
+    var myExp = new RegExp(searchField, "i");
 
-$.getJSON("WCA_Results_Person_India.json", function(data){
-    var output = '<ol>';
-    // var index = 1;
-    $.each(data, function (key, val) {
-        output += '<li><p>'+val.personName +' participated in ' +val.eventId
-                + ' with an average speed '+(val.average/60).toFixed(3)+' secs</p></li>';        
-        
+    $.getJSON("WCA_Results_Person_India.json", function(data){
+        var output = '<ol class="searchResults">';
+        // var index = 1;
+        $.each(data, function (key, val) {
+            if(val.personName.test(myExp)){
+                output += '<p><li>'+
+                            val.personName +' participated in ' +val.eventId + 
+                            ' with an average speed '+(val.average/60).toFixed(3)
+                        +' secs</li></p>';
+            }
+        });
+        output += '</ol>';
+        $('.orderedList').html(output);
     });
-    output += '</ol>';
-    $('.orderedList').html(output);
 });
+
+
 
 // $(".orderedList").load(APIendpoint[2]);
